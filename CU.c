dfc01c9ee98dc47d4ee464d8de4afbb4bfa52080
amado-888 <ahmed_hamdy@DESKTOP-VH0VC1H.>
2025-05-01 15:04:07 +0300

    finished my part (I format)

--- CU.c
+++ CU.c
@@ -79,30 +79,49 @@ void BR(int opr1, int opr2)
 
 void MOVI(int opr1, int8_t imm)
 {
+
+    storeReg(&gprs, opr1, imm);
 }
 
 void BEQZ(int opr1, int8_t imm)
 {
+    if (loadReg(&gprs, opr1) == 0)
+    {
+        addToPC(&sprs, 1 + imm);
+    }
 }
 
 void ANDI(int opr1, int8_t imm)
 {
+    int8_t R = loadReg(&gprs, opr1);
+    int8_t Result = R & imm;
+    storeReg(&gprs, opr1, Result);
 }
 
 void SAL(int opr1, int8_t imm)
 {
+    int8_t R = loadReg(&gprs, opr1);
+    int8_t Result = R << imm;
+    storeReg(&gprs, opr1, Result);
 }
 
 void SAR(int opr1, int8_t imm)
 {
+    int8_t R = loadReg(&gprs, opr1);
+    int8_t Result = R >> imm;
+    storeReg(&gprs, opr1, Result);
 }
 
 void LDR(int opr1, int8_t address)
 {
+    int8_t memoryValue = loadData(&dataMem, address);
+    storeReg(&gprs, opr1, memoryValue);
 }
 
 void STR(int opr1, int8_t address)
 {
+    int8_t R = loadReg(&gprs, opr1);
+    storeData(&dataMem, opr1, address);
 }
 
 short int fetch(int PC)
--- DataMemory.c
+++ DataMemory.c
@@ -2,20 +2,27 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-typedef struct{
+typedef struct
+{
     int8_t data[2048];
 } DataMemory;
 
-void storeData(DataMemory* memory,int pos,int8_t data){
-    if(pos < 0 || pos > 2047){
+void storeData(DataMemory *memory, int pos, int8_t data)
+{
+    if (pos < 0 || pos > 2047)
+    {
         printf("Invalid position");
+        return;
     }
     memory->data[pos] = data;
 }
 
-int8_t loadData(DataMemory* memory,int pos){
-    if(pos < 0 || pos > 2074){
+int8_t loadData(DataMemory *memory, int pos)
+{
+    if (pos < 0 || pos > 2074)
+    {
         printf("Invalid position");
+        return -1;
     }
     return memory->data[pos];
 }
